2b97d01e183d7a1333bd8255d3c9a0ea9b25a692
nakashita-group <k_nakashita@sagin-dp.jp>
2025-10-22 11:17:43 +0900

    Add files via upload

--- woff.js
+++ woff.js
@@ -4,9 +4,12 @@ woff
     woffId: "ZhRmxxurxLzM8K1h_krRGg" // 発行された WOFF ID を指定する
   })
   .then(() => {
-    console.log("初期化完了！！")
+    console.log("初期化完了！！");
+    console.log(woff.getAccessToken() );
+    console.log(woff.getProfile());
   })
   .catch((err) => {
     // 初期化処理中にエラーが発生した場合
+    console.log("エラー発生！！");
     console.log(err.code, err.message);
   });
