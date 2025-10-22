2d2d50939b18fa9e1749f876714aa54323bbffb0
nakashita-group <k_nakashita@sagin-dp.jp>
2025-10-22 15:17:59 +0900

    Add files via upload

--- sendMessage.js
+++ sendMessage.js
@@ -4,8 +4,9 @@ document.getElementById("sendMessage").addEventListener("click", function () {
     document.getElementById("message").innerText="clicked";
     const UserName = document.getElementById("UserName").innerText;
     let strWorkTime = document.getElementById("StrWorkTime").value;
+    let endWorkTime = document.getElementById("EndWorkTime").value;
     woff.sendMessage({
-        content: `${UserName}:${strWorkTime}`
+        content: `${UserName}:開始時間${strWorkTime}~終了時間:${endWorkTime}`
     })
         .then(() => {
             console.log("メッセージ送信完了");
