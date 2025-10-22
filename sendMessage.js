20dc2fc48be2fa57417136ebb023a42084ff1deb
nakashita-group <k_nakashita@sagin-dp.jp>
2025-10-22 14:32:09 +0900

    Add files via upload

--- sendMessage.js
+++ sendMessage.js
@@ -1,5 +1,17 @@
-document.getElementById("sendMessage").addEventListener("click",function(){
-    console.log(woff.getProfile());
+
+
+document.getElementById("sendMessage").addEventListener("click", function () {
+    const UserName = document.getElementById("UserName").innerText;
+    let strWorkTime = document.getElementById("StrWorkTime").value;
+    woff.sendMessage({
+        content: `${UserName}:${strWorkTime}`
+    })
+        .then(() => {
+            console.log("メッセージ送信完了");
+        })
+        .catch((err) => {
+            console.log('error', err);
+        });
+
 });
 
-const UserName=document.getElementById("UserName").innerText;
